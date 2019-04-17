154e386beae46306f31b688b250f2d5416c64591
areeba-waheed <areeba.waheed.97@gmail.com>
2019-04-16 20:27:39 -0700

    added more tests

--- selenium/src/co/areebs/selenium/demo/basic/LaunchAppinChrome.java
+++ selenium/src/co/areebs/selenium/demo/basic/LaunchAppinChrome.java
@@ -1,5 +1,9 @@
 package co.areebs.selenium.demo.basic;
+import static org.junit.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
+
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class LaunchAppinChrome {
 
@@ -29,8 +34,8 @@ public class LaunchAppinChrome {
 		chromeDriver.navigate().to("http://eventsplanner.azurewebsites.net/");
 		
 		testTitle(chromeDriver);
-		//testLogin(chromeDriver);
-		testRegister(chromeDriver);
+		testLogin(chromeDriver);
+		//testRegister(chromeDriver);
 		testUpdateProfileInfo(chromeDriver);
 		testEvents(chromeDriver);
 		
@@ -53,14 +58,18 @@ public class LaunchAppinChrome {
 	}
 
 	private static void testTitle(WebDriver driver) throws InterruptedException {
-		String title = driver.getTitle();
+		String title = driver.getTitle();		
+		String actualTitle = "Events Planner";		
+		assertEquals(title, actualTitle);
 		
-		if(title.equalsIgnoreCase("Events Planner")) {
+		/*if(title.equalsIgnoreCase("Events Planner")) {
 			System.out.println("Title Matches");
 		}else {
 			System.out.println(title);
 		}
-		Thread.sleep(2000);
+		Thread.sleep(2000);*/
+		
+		
 		
 	}
 
@@ -69,9 +78,38 @@ public class LaunchAppinChrome {
 		
 	}
 
-	private static void testUpdateProfileInfo(WebDriver driver2) {
-		// TODO Auto-generated method stub
+	private static void testUpdateProfileInfo(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
 		
+		WebElement dropdown = driver.findElement(By.cssSelector("body > app-root > div > app-nav > nav > div > div > a"));
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(dropdown);
+		action.click();
+		action.perform();
+		Thread.sleep(500);
+		WebElement edit = driver.findElement(By.cssSelector("body > app-root > div > app-nav > nav > div > div > div > a.dropdown-item.fa.fa-user"));
+		action.moveToElement(edit);
+		action.click();
+		action.perform();
+		
+		WebElement lastName = driver.findElement(By.cssSelector("#lastName"));
+		Thread.sleep(500);
+		lastName.clear();
+		Thread.sleep(500);
+		lastName.sendKeys("Dobby");
+		Thread.sleep(1000);
+		WebElement save = driver.findElement(By.cssSelector("body > app-root > div > app-member-edit > div > div:nth-child(2) > div.col-sm-4 > div > div.card-footer > div > button"));
+		Actions actionReg = new Actions(driver);
+		actionReg.moveToElement(save);
+		actionReg.click();
+		actionReg.perform();
+		Thread.sleep(1000);
+		
+		String message = driver.getCurrentUrl();
+		String actual = "https://eventsplanner.azurewebsites.net";
+		//assertEquals(message, actual);
+		//System.out.println(driver.getWindowHandle());
 	}
 
 	private static void testRegister(WebDriver driver) throws InterruptedException {
@@ -116,20 +154,31 @@ public class LaunchAppinChrome {
 		Actions actionReg = new Actions(driver);
 		actionReg.moveToElement(registerbttn);
 		actionReg.click();
-		actionReg.perform();*/
+		actionReg.perform();
+		
+		String message = driver.getCurrentUrl();
+		String actual = "https://eventsplanner.azurewebsites.net/members";
+		assertEquals(message, actual);
+		*/
+		
+		
 		
 		WebElement cancel = driver.findElement(By.cssSelector("body > app-root > div > app-home > div > div > div > div > app-register > form > div.form-group.text-center > button.btn.btn-danger"));
 		Actions actionCancel = new Actions(driver);
 		actionCancel.moveToElement(cancel);
 		actionCancel.click();
 		actionCancel.perform();
-		
+		Thread.sleep(1000);
+		String message = driver.getCurrentUrl();
+		String actual = "https://eventsplanner.azurewebsites.net";
+		assertEquals(message, actual);
+		//driver.quit();
 		
 	}
 
 	private static void testLogin(WebDriver driver) throws InterruptedException {
 		WebElement findUsername = driver.findElement(By.name("Username"));
-		findUsername.sendKeys("areebs");
+		findUsername.sendKeys("aribers");
 		Thread.sleep(1000);
 		WebElement findPassword = driver.findElement(By.name("Password"));
 		findPassword.sendKeys("poop123");
@@ -139,6 +188,12 @@ public class LaunchAppinChrome {
 		action.moveToElement(login);
 		action.click();
 		action.perform();
+		Thread.sleep(1000);
+		String message = driver.getCurrentUrl();
+		String actual = "https://eventsplanner.azurewebsites.net/events";
+		assertEquals(message, actual);
+		
+		//driver.quit();
 		
 	}
 
